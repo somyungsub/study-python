53abc1b635a1cd8042e32ea2b88f71d8a3dec7b9
somyungsub <gkdldy5@naver.com>
2018-06-04 11:01:14 +0900

    Add - 02.자료형과 연산 예제

---
+++ 02.자료형과 연산/01.test.py
@@ -0,0 +1,29 @@
+a = 3
+b = 2
+print(a + b);
+print(a - b);
+print(a * b);
+print(a / b);
+print(a % b);
+print(a ** b);
+print(a // b);
+
+if a > 10:
+    print(a + 10)
+else:
+    if a > 2:
+        print(a * b)
+    else:
+        a = 5
+        print('a == ', a)
+print(a * b)
+print(type(10))
+print(type(10.1))
+print(type('b'))
+print(id(a))
+print(id(b))
+
+print(isinstance(a, int))
+print(isinstance(a, float))
+
+
