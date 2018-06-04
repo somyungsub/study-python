98e024b9ef4448d2a7099ceaa943e3d001578f5d
somyungsub <gkdldy5@naver.com>
2018-06-04 18:02:20 +0900

    Add - 02.자료형과 연산 예제

--- 02.자료형과 연산/01.test.py
+++ 02.자료형과 연산/01.test.py
@@ -20,10 +20,46 @@ print(a * b)
 print(type(10))
 print(type(10.1))
 print(type('b'))
+
+print(id(a))
+a = 5;  # 상수객체 5를 참조한다 -> id가 변경됨
+
 print(id(a))
 print(id(b))
 
 print(isinstance(a, int))
 print(isinstance(a, float))
+print("-------------------------")
 
+# 같은 객체 공간 반환
+c = d = []
+
+print(id(c))
+print(id(d))
+
+# 다른 객체 공간 반환
+c = []
+d = []
+print(id(c))
+print(id(d))
+print(type(c))
+print(type(d))
+
+print(c == d)   # True : 값 비교
+print(id(c) == id(d))   # False : 고유번호 비교
+
+c.append(1)
+d.append(1)
+
+print(id(c[0]))
+print(id(d[0]))
+print(id(1))
+
+del a
+print(dir())
+print(globals())
+print(locals())
+# print(a)
+
+print(1, 2, 3, sep='.', end='?'); print(1, 2, 3, sep='/', end='!');print(1, 2, 3, sep=',', end='g');
 
